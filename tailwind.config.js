d99679293935a54655bc35e419ae4bbc0ff29454
ayati <ayatirauthan@gmail.com>
2025-08-11 15:23:25 +0000

    configured tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.js"], // Scan all HTML files in root and JS files in src/
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
